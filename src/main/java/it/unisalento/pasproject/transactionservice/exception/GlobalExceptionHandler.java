2fd2148ccbc8af5338ad770b57eed9024d97042f
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-06-17 09:22:57 +0200

    fix: Access denied exception

--- src/main/java/it/unisalento/pasproject/transactionservice/exception/GlobalExceptionHandler.java
+++ src/main/java/it/unisalento/pasproject/transactionservice/exception/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.security.access.AccessDeniedException;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
